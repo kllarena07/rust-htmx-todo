fd41da4f3e40e5ffbca894664d7e2b64ed070640
Kieran Llarena <baymax709@gmail.com>
2024-06-13 20:58:07 -0400

    feat: serve 200 on localhost port 3000

---
+++ backend/src/main.rs
@@ -0,0 +1,32 @@
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::Read;
+use dotenv::dotenv;
+use std::env;
+
+fn main() {
+    dotenv().ok();
+
+    let port: String = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+ 
+    let tcp_addr: String = format!("127.0.0.1:{}", port);
+
+    let listener = TcpListener::bind(tcp_addr).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
