d1b8f655643d4ffee805c2878b672c23afdda78e
Kieran Llarena <baymax709@gmail.com>
2024-06-14 12:45:19 -0400

    feat: print read failure message

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -24,7 +24,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).expect("Error: Failed to read from client.");
     
     let response = "HTTP/1.1 200 OK\r\n\r\n";
     stream.write(response.as_bytes()).unwrap();
