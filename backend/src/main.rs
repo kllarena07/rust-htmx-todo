54f915e817faa89200e99f270dcd448208657c59
Kieran Llarena <baymax709@gmail.com>
2024-06-17 12:32:37 -0400

    feat: serve HTML on request

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,7 +10,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).expect("Error: Failed to read from client.");
     
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("../frontend/index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
